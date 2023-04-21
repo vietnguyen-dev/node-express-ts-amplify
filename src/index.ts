ec3b3e4fbe238fd1cff71e446fddf6c5a2bf8a19
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-04-20 20:06:05 -0700

    hellop there

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ dotenv.config()
 const port = process.env.PORT
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World amplify hosint!');
+  res.send('Hello World amplify nt!');
 });
 
 app.listen(port, () => {
