978c8f2b26f7f530b1a4bfdeacc68438a2fc4a2a
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-04-20 19:06:13 -0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, {Application, Request, Response } from 'express';
+import dotenv from 'dotenv'
+
+const app: Application = express();
+dotenv.config()
+
+const port = process.env.PORT
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
